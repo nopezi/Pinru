acde9b4cd1c94b2b22ef5b3f78f5e1f7de439536
nopezi saputra <snopezi@gmail.com>
2019-08-15 11:45:44 +0700

    ganti icon

--- application/views/meeting_rule.php
+++ application/views/meeting_rule.php
@@ -12,9 +12,9 @@
                         <div class="col-sm-11"><p class="h6"> Mulai dengan Wise dan Foqual Contact</p></div>
                         <div class="col-sm-1"><i class="fas fa-chair"></i></div>
                         <div class="col-sm-11"><p class="h6"> Rapihkan Meja dan Kursi</p></div>
-                        <div class="col-sm-1"><i class="fas fa-broom"></i></div>
+                        <div class="col-sm-1"><i class="fas fa-trash-alt"></i></div>
                         <div class="col-sm-11"><p class="h6 text-left"> Bersihkan Meja & Buanglah sampah pada tempatnya</p></div>
-                        <div class="col-sm-1"><i class="fas fa-power-off"></i></div>
+                        <div class="col-sm-1"><i class="fas fa-plug"></i></div>
                         <div class="col-sm-11"><p class="h6"> Matikan Lampu, AC dan LCD / Monitor</p></div>
                     </div>
                 </div>
